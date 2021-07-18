90bec7b1b4327168fde7f0f4d909c64389288866
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-07-18 20:29:51 -0300

    Style: new explore icon

--- src/common/components/Footer.jsx
+++ src/common/components/Footer.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import store, { setFetchOnDone } from '../../context/store';
 import drinkIcon from '../../images/drinkIcon.png';
-import exploreIcon from '../../images/exploreIcon.svg';
+import exploreIcon from '../../images/exploreIcon.png';
 import mealIcon from '../../images/mealIcon.png';
 
 export default function Footer() {
