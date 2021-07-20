5518510d5de4ba87016959cd5bbbe11ec224229a
Thiago Felipe <tfeliperibeiro@pop-os.localdomain>
2021-07-20 18:43:38 -0300

    fix: CSS pagina receitas favoritas

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -59,37 +59,47 @@ class FavoriteRecipes extends React.Component {
       return (
         <section key={ recipe.id }>
           <Link to={ link }>
-            <img
-              data-testid={ `${index}-horizontal-image` }
-              src={ recipe.image }
-              alt={ recipe.name }
-              width="180"
-            />
+            <div className="container-img-favorite">
+              <img
+                data-testid={ `${index}-horizontal-image` }
+                src={ recipe.image }
+                alt={ recipe.name }
+                className="img-favorite"
+              />
+            </div>
           </Link>
-          <p data-testid={ `${index}-horizontal-top-text` }>
+          <p className="text-recipe" data-testid={ `${index}-horizontal-top-text` }>
             {recipe.type === 'comida' ? recipeInfo : recipe.alcoholicOrNot }
           </p>
-          <Link to={ link }>
+          <Link className="link-favorite" to={ link }>
             <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
           </Link>
           <p>{ copyLink ? 'Link copiado!' : null }</p>
-          <button
-            type="button"
-            onClick={ () => this.onClickShare(link) }
-          >
-            <img
-              data-testid={ `${index}-horizontal-share-btn` }
-              src={ shareIcon }
-              alt="Compartilhar"
-            />
-          </button>
-          <button type="button" onClick={ () => this.onClickFavoriteIcon(recipe.id) }>
-            <img
-              src={ blackHeartIcon }
-              alt="favorito"
-              data-testid={ `${index}-horizontal-favorite-btn` }
-            />
-          </button>
+          <div className="container-share-favorite">
+            <button
+              type="button"
+              onClick={ () => this.onClickShare(link) }
+              className="share-btn"
+            >
+              <img
+                data-testid={ `${index}-horizontal-share-btn` }
+                src={ shareIcon }
+                alt="Compartilhar"
+              />
+            </button>
+            <button
+              type="button"
+              onClick={ () => this.onClickFavoriteIcon(recipe.id) }
+              className="favorite-btn"
+            >
+              <img
+                src={ blackHeartIcon }
+                alt="favorito"
+                data-testid={ `${index}-horizontal-favorite-btn` }
+              />
+            </button>
+          </div>
+          <hr className="line-favorite" />
         </section>
       );
     });
@@ -107,27 +117,32 @@ class FavoriteRecipes extends React.Component {
     return (
       <section>
         <Header title="Receitas Favoritas" />
-        <button
-          data-testid="filter-by-all-btn"
-          type="button"
-          onClick={ this.getFavoriteRecipes }
-        >
-          All
-        </button>
-        <button
-          data-testid="filter-by-food-btn"
-          type="button"
-          onClick={ () => this.renderFilterFavoriteRecipe('comida') }
-        >
-          Food
-        </button>
-        <button
-          data-testid="filter-by-drink-btn"
-          type="button"
-          onClick={ () => this.renderFilterFavoriteRecipe('bebida') }
-        >
-          Drinks
-        </button>
+        <div className="container-btn">
+          <button
+            data-testid="filter-by-all-btn"
+            type="button"
+            onClick={ this.getFavoriteRecipes }
+            className="main-btn"
+          >
+            All
+          </button>
+          <button
+            data-testid="filter-by-food-btn"
+            type="button"
+            onClick={ () => this.renderFilterFavoriteRecipe('comida') }
+            className="main-btn"
+          >
+            Food
+          </button>
+          <button
+            data-testid="filter-by-drink-btn"
+            type="button"
+            onClick={ () => this.renderFilterFavoriteRecipe('bebida') }
+            className="main-btn"
+          >
+            Drinks
+          </button>
+        </div>
         {this.renderFavoriteRecipes()}
       </section>
     );
